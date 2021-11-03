3b715bfc68a5989edde91913d2c047a94bb7e9fa
Gabriel Calarco <gabcalarco@gmail.com>
2021-11-03 16:32:31 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f310fcaf651753062e06.js"
+    "url": "webpack-runtime-6492091031a1955b031d.js"
   },
   {
     "url": "framework-beda7bde9d7dca0755ce.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3682ba8417594783449d336ba27e476b"
+    "revision": "bbf7826034d2b7c895b715e2fd54c429"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "95207d5840f56248058546632a691171"
+    "revision": "703fe96c1c5d2fd55b4f709338054e91"
   },
   {
     "url": "polyfill-79c37fe16fed14a2ac3b.js"
